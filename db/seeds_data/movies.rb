33c0acabfaf41534736c507f4ab8d8885de8171d
Mr.Rene <venelrene@gmail.com>
2015-01-30 13:11:48 -0500

    Added some seed for data

--- db/seeds_data/movies.rb
+++
@@ -1,5 +0,0 @@
-require 'csv'
-
-CSV.foreach(Rails.root.join("db/seeds_data/movies.rb"), headers:true) do |row
-  Movie.find_or_create_by(title: row[0], release_year: row[1], price: row[2], description: row[3], imdb_id: row[4], poster_url: row[5] )
-end
