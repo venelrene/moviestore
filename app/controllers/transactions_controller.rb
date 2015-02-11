076481a1670da2517c41d7001ce3d28ba06b159b
Mr.Rene <venelrene@gmail.com>
2015-02-11 12:57:02 -0500

    Update the create action to store the new customer

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,7 +6,7 @@ class TransactionsController < ApplicationController
   end
 
   def new
-    gon.client_token = generate_new_client_token
+    gon.client_token = generate_client_token
   end
 
   def edit
@@ -15,23 +15,44 @@ class TransactionsController < ApplicationController
   end
 
   def create
-    @result = Braintree::Transaction.sale(
-        amount: current_user.cart_total_price,
-        payment_method_nonce: params[:payment_method_nonce])
+    unless current_user.has_payment_info?
+      @result = Braintree::Transaction.sale(
+          amount: current_user.cart_total_price,
+          payment_method_nonce: params[:payment_method_nonce],
+          customer: {
+              first_name: params[:first_name],
+              last_name: params[:last_name],
+              company: params[:company],
+              email: current_user.email,
+              phone: params[:phone]
+          },
+          options: {
+              store_in_vault: true
+          })
+    else
+      @result = Braintree::Transaction.sale(
+          amount: current_user.cart_total_price,
+          payment_method_nonce: params[:payment_method_nonce])
+    end
+
     if @result.success?
+      current_user.update(braintree_customer_id: @result.transaction.customer_details.id) unless current_user.has_payment_info?
       current_user.purchase_cart_movies!
-      redirect_to root_url, notice: "Congraulations! You've purchased new movies successfully!"
+      redirect_to root_url, notice: "Congraulations! Your transaction has been successfully!"
     else
       flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
-      gon.client_token = generate_new_client_token
+      gon.client_token = generate_client_token
       render :new
     end
   end
 
   private
-
-  def generate_new_client_token
-    Braintree::ClientToken.generate
+ def generate_client_token
+    if current_user.has_payment_info?
+      Braintree::ClientToken.generate(customer_id: current_user.braintree_customer_id)
+    else
+      Braintree::ClientToken.generate
+    end
   end
 
 end
